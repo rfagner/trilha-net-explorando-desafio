705a766e08f5cb085982bab0f80f956942a6eaf0
Renildo Fagner <renildo.fagner@gmail.com>
2023-09-27 13:19:01 -0300

    Implementadas melhorias no programa, incluindo tratamento de exceções, formatação de moeda para a diária e outras otimizações.

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -14,17 +14,14 @@ namespace DesafioProjetoHospedagem.Models
         }
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
-        {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
+        {                        
+            if (hospedes.Count <= Suite.Capacidade)
             {
                 Hospedes = hospedes;
             }
             else
-            {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+            {                
+                throw new Exception("A capacidade da suíte não é suficiente para o número de hóspedes.");
             }
         }
 
@@ -34,24 +31,17 @@ namespace DesafioProjetoHospedagem.Models
         }
 
         public int ObterQuantidadeHospedes()
-        {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+        {            
+            return Hospedes.Count;
         }
 
         public decimal CalcularValorDiaria()
-        {
-            // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
-
-            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
-            {
-                valor = 0;
+        {            
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+            
+            if (DiasReservados >= 10)
+            {                
+                valor *= 0.9m; // Desconto de 10% se a reserva for acima de 10 dias
             }
 
             return valor;
